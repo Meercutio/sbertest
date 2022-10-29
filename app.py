60c2490e214e40e3c6465a9286610e9c4863c0a9
Janita Lucile <jlucile@at-e3.msk.21-school.ru>
2022-10-29 22:49:10 +0300

    try 5

--- app.py
+++ app.py
@@ -1,6 +1,30 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
+from starlette import status
+from fastapi.exception_handlers import (
+    http_exception_handler,
+    request_validation_exception_handler,
+)
+from fastapi.exceptions import RequestValidationError
 
 from api import router
 
 app = FastAPI()
 app.include_router(router)
+
+
+@app.exception_handler(Exception)
+async def validation_exception_handler(request: Request, exc: Exception):
+    return JSONResponse(
+        status_code=status.HTTP_400_BAD_REQUEST,
+        content=jsonable_encoder({"error": f'{exc}'}),
+    )
+
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: Exception):
+    return JSONResponse(
+        status_code=status.HTTP_400_BAD_REQUEST,
+        content=jsonable_encoder({"error": f'{exc}'}),
+    )
