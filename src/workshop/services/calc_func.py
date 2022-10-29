9f801018001fbc24df3bcb9b694532564448872c
Janita Lucile <jlucile@at-e3.msk.21-school.ru>
2022-10-29 20:08:05 +0300

    add 2nd gen

--- src/workshop/services/calc_func.py
+++ src/workshop/services/calc_func.py
@@ -1,22 +1,31 @@
 from ..models import CalculationBody
-from datetime import date, timedelta
-import calendar
+from datetime import datetime, date
+import logging
+from dateutil.relativedelta import relativedelta
 
 
-def iter_month(current_date: date):
-    days = calendar.monthrange(current_date.year, current_date.month)[1]
-    next_month_date = current_date + timedelta(days=days)
-    return next_month_date
+logger = logging.getLogger("log")
+logger.setLevel("INFO")
 
 
-def main_calculation(body: CalculationBody):
-    start_date: date = body.date
+def calculate_amount(amount: float, rate: float) -> float:
+    payment: float = amount
+    return round(payment * (1 + rate/12/100), 2)
+
+
+def main_calculation(body: CalculationBody) -> dict:
+    format_time = "%d.%m.%Y"
+    start_date: date = datetime.strptime(body.date, format_time)
     periods: int = body.periods
     amount: int = body.amount
-    rate: int = body.rate
-    result = {start_date: amount}
+    rate: float = body.rate
+    tmp_amount: float = amount
+
+    result = {}
     i = 0
     while i < periods:
-        result[start_date + timedelta(days=i)] = amount
+        temp_data = start_date + relativedelta(months=+i)
+        tmp_amount = calculate_amount(tmp_amount, rate)
+        result[temp_data.strftime('%d.%m.%Y')] = tmp_amount
         i += 1
     return result
