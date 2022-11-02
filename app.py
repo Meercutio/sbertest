9ed398b9701bb2f878a93eec78534273383026ae
Janita Lucile <jlucile@at-i3.msk.21-school.ru>
2022-11-02 14:16:55 +0300

    fixed issues

--- app.py
+++ app.py
@@ -9,14 +9,6 @@ app = FastAPI()
 app.include_router(calc_router)
 
 
-@app.exception_handler(Exception)
-async def validation_exception_handler(request: Request, exc: Exception):
-    return JSONResponse(
-        status_code=status.HTTP_400_BAD_REQUEST,
-        content=jsonable_encoder({"error": f'{exc}'}),
-    )
-
-
 @app.exception_handler(RequestValidationError)
 async def validation_exception_handler(request: Request, exc: Exception):
     return JSONResponse(
--- dockerfile
+++ dockerfile
@@ -6,14 +6,10 @@ COPY ./ ./
 
 RUN pip install --upgrade pip
 
-RUN python3.10 -m venv .env
-
-
 RUN pip install --no-cache-dir -r requirements.txt
 
 EXPOSE 8000
 
 RUN python -m pytest && coverage run -m pytest && coverage report -m
-
 
 CMD ["uvicorn", "app:app", "--proxy-headers", "--host", "0.0.0.0", "--port", "8000"]
